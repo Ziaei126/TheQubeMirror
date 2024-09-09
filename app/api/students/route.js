c394764e5d4d04df14d8910e1d85dc0771650c0b
Hasan Shahrestani <124054055+smhs4@users.noreply.github.com>
2024-09-09 01:32:51 +0100

    revalidation

--- app/api/students/route.js
+++ app/api/students/route.js
@@ -1,38 +1,39 @@
 import prisma from '/lib/prisma';
 import { NextResponse } from 'next/server';
 import { unstable_cache } from 'next/cache';
+
 const termId = parseInt(process.env.TERM_ID);
 
 const getCachedRegistrations = unstable_cache(
-    async () => {
-      return await prisma.registration.findMany({
-        where: {
-            term_id: termId,
-        },
-        include: {
-            student: true,  // Include student details
-            islamic: true,  // Include the Islamic course
-            skill: true,    // Include the Skill course
-            language: true, // Include the Language course
-            sport: true,    // Include the Sport course
-        },
+  async () => {
+    return await prisma.registration.findMany({
+      where: {
+        term_id: termId,
+      },
+      include: {
+        student: true,  // Include student details
+        islamic: true,  // Include the Islamic course
+        skill: true,    // Include the Skill course
+        language: true, // Include the Language course
+        sport: true,    // Include the Sport course
+      },
     });
-    },
-    ['posts'],
-    { tags: ['term_register'] }
-  )
+  },
+  { tags: ['term_register'] }
+);
 
-export async function GET(req, res) {
+export async function GET(req) {
+  try {
+    // You need to await the result of the cached function
+    const registrations = await getCachedRegistrations();
     
+    // Log the registrations for debugging
+    console.log(registrations);
 
-    try {
-        const registrations = getCachedRegistrations()
-
-        return NextResponse.json(registrations);
-    } catch (error) {
-        console.error("Failed to fetch registrations:", error);
-        return new NextResponse('Internal Server', { status: 500 });
-    }
-
-
+    // Return a JSON response with the registrations
+    return NextResponse.json(registrations);
+  } catch (error) {
+    console.error("Failed to fetch registrations:", error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
 }
