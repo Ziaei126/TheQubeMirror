2bae056166d96c743e85833013d42d89bb7c8501
Hasan Shahrestani <124054055+smhs4@users.noreply.github.com>
2025-01-09 15:16:03 +0000

    sign in button updated

--- app/components/navigation/SignInButton.js
+++ app/components/navigation/SignInButton.js
@@ -1,25 +1,41 @@
 'use client'
 
-import { signIn, signOut ,useSession} from 'next-auth/react'
-
-
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { usePathname } from 'next/navigation'
+import Link from '@node_modules/next/link'
 
 function SignInButton() {
-    const { data: session } = useSession()
+  const { data: session } = useSession()
+  const pathname = usePathname()
 
-    // Check if the user is signed in
-  const isSignedIn = session ? true : false;
-
+  // Check if the user is signed in and on the dashboard page
+  const isDashboardPage = pathname === '/dashboard'
 
   return (
-    <div >
-      {isSignedIn ? (
-        // User is signed in
-        
-        <button onClick={() => signOut() } className="p-2 shadow rounded bg-slate-200 hover:shadow-none">{`Sign Out`}</button>
+    <div>
+      {session ? (
+        !(session.isStaff) || isDashboardPage ? (
+          // Show "Sign Out" button only on the dashboard page
+          <button
+            onClick={() => signOut()}
+            className="p-2 shadow rounded bg-slate-200 hover:shadow-none"
+          >
+            Sign Out
+          </button>
+        ) : <Link
+        href='/dashboard'
+        className="p-2 shadow rounded bg-slate-200 hover:shadow-none"
+      >
+        Dashboard
+      </Link> // No button outside the dashboard page
       ) : (
-        // User is not signed in
-        <button onClick={() => signIn() } className="p-2 shadow rounded bg-slate-200 hover:shadow-none">Sign In</button>  // Change the URL to your actual sign-in page
+        // Show "Sign In" button if not signed in
+        <button
+          onClick={() => signIn()}
+          className="p-2 shadow rounded bg-slate-200 hover:shadow-none"
+        >
+          Sign In
+        </button>
       )}
     </div>
   )
